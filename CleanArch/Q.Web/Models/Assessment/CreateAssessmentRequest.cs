1b182ba74a553b2ffe36336660799a5d17d2df1f
Vijay Kumar <vjdeveloper29@gmail.com>
2018-07-26 18:10:57 +0100

    changes

---
+++ CleanArch/Q.Web/Models/Assessment/CreateAssessmentRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Q.Web.Models.Assessment
+{
+    public class CreateAssessmentRequest : BaseModel
+    {
+        public string Title { get; set; }
+        public DateTime? AssessmentDate { get; set; }
+
+        public int AssessmentTypeId { get; set; }
+
+        public string Reference { get; set; }
+
+        public int? ScopeId { get; set; }
+
+        public bool Published { get; set; }
+    }
+}
